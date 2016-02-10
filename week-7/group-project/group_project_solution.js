b8a58e96490148914a4d99ae10b0193616c1b046
torihuang <victoriahuang20@gmail.com>
2016-02-09 22:31:10 -0600

    Complete person1 tasks

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,9 @@
+/*
+PERSON 1 - TORI HUANG
+
+As a user, I want three functions that will take my collection of numbers and complete calculations for me. I have two collections of numbers. The first is 3,3,3,4,4,4,6 and the second is 5,5,5,5,6,6,8,8. I've listed the requirements for calculations below:
+
+ - A function called 'sum' that will tell me the total value of my numbers added together.
+ - A function called 'mean' that returns the average value of my numbers.
+ - A function called 'median' that returns the median value of my numbers.
+*/

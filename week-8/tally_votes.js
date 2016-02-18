f5a1b005ed5ce67cbcacbe19c8bd0fd6b43e3342
torihuang <victoriahuang20@gmail.com>
2016-02-18 13:57:58 -0600

    Complete pairs 8.2 and 8.3

--- week-8/tally_votes.js
+++ week-8/tally_votes.js
@@ -3,6 +3,18 @@
 // I worked on this challenge with:
 // This challenge took me [#] hours.
 
+// PSEUDOCODE
+// Input: An OBJECT containing people and their votes
+// Output: An OBJECT of elected positions
+// Steps:
+//  VOTE COUNT
+//   1. Iterate through every OBJECT in the votes collection
+//   2. Access key (name) from votes for each elected position
+//   3. Fill voteCount with names of elected people as the property and the number of votes they received as the value.
+//  DETERMINE WINNERS
+//   1. Iterate through voteCount (president, vp, secretary, treasurer)
+//   2. Fill officers with highest value count as you iterate.
+
 // These are the votes cast by each student. Do not alter these objects here.
 var votes = {
   "Alex": { president: "Bob", vicePresident: "Devin", secretary: "Gail", treasurer: "Kerry" },
@@ -41,20 +53,16 @@ var voteCount = {
   treasurer: {}
 }
 
-/* The name of each student receiving a vote for an office should become a property
-of the respective office in voteCount.  After Alex's votes have been tallied,
-voteCount would be ...
-  var voteCount = {
-    president: { Bob: 1 },
-    vicePresident: { Devin: 1 },
-    secretary: { Gail: 1 },
-    treasurer: { Kerry: 1 }
-  }
-*/
+// var voteCount = {
+//   president: {
+//             "Bob": 5,
+//             "Devin:"3,
+// },
+//   vicePresident: {},
+//   secretary: {},
+//   treasurer: {}
+// }
 
-
-/* Once the votes have been tallied, assign each officer position the name of the
-student who received the most votes. */
 var officers = {
   president: undefined,
   vicePresident: undefined,
@@ -62,37 +70,66 @@ var officers = {
   treasurer: undefined
 }
 
-// Pseudocode
+var officer_vote_count = {
+  president: 0,
+  vicePresident: 0,
+  secretary: 0,
+  treasurer: 0
+}
+
+// INITIAL SOLUTION
+
+// for (var student in votes) {
+//   // console.log(votes[student]);
+//   // console.log(student.president);
+//  for (var position in votes[student]) {
+//    // console.log(votes[student][position]);
+//    if (voteCount[position].hasOwnProperty(votes[student][position])) {
+//      voteCount[position][votes[student][position]] += 1;
+//    } else {
+//       voteCount[position][votes[student][position]] = 1;
+//    }
+
+//    if (voteCount[position][votes[student][position]] > officer_vote_count[position]){
+//    officer_vote_count[position] = voteCount[position][votes[student][position]];
+//    officers[position] = votes[student][position];
+//    // Fill officer_vote_count with greater value count
+
+//    }
+//  }
+
+// };
+
+// REFACTORED SOLUTION
+
+for (var student in votes) {
+  // console.log(votes[student]);
+  // console.log(student.president);
+ for (var position in votes[student]) {
+   // console.log(votes[student][position]);
+   var candidate_choice = votes[student][position];
+   if (voteCount[position].hasOwnProperty(candidate_choice)) {
+     voteCount[position][candidate_choice] += 1;
+   } else {
+      voteCount[position][candidate_choice] = 1;
+   }
+
+   if (voteCount[position][candidate_choice] > officer_vote_count[position]){
+   officer_vote_count[position] = voteCount[position][candidate_choice];
+   officers[position] = candidate_choice;
+   }
+ }
+
+};
 
 
-// __________________________________________
-// Initial Solution
+
+console.log(voteCount);
+console.log(officer_vote_count);
 
 
 
-
-
-
-
-// __________________________________________
-// Refactored Solution
-
-
-
-
-
-
-// __________________________________________
-// Reflection
-
-
-
-
-
-
-// __________________________________________
-// Test Code:  Do not alter code below this line.
-
+// TESTS
 
 function assert(test, message, test_number) {
   if (!test) {
@@ -150,3 +187,17 @@ assert(
   "Ivy should be elected Treasurer.",
   "8. "
 )
+
+// REFLECTION
+
+/*
+
+What did you learn about iterating over nested objects in JavaScript?
+When you use for var x in object, you are actually calling the position of x in the object. x will be a string!
+
+Were you able to find useful methods to help you with this?
+We utilized #hasOwnProperty, it is a very helpful method that checks if an object has a property.
+
+What concepts were solidified in the process of working through this challenge?
+My concept of iterating through nested objects was solidified in this challenge!s
+*/
